c0a59fdc7dbf8d83161582fb048e506965f63ade
jalotra <jalotrashivam9@gmail.com>
2019-11-07 16:21:00 +0530

    Added methods to take images inputs

---
+++ input_data/data.h
@@ -0,0 +1,15 @@
+// Defines the data batch
+
+typedef struct 
+{	
+	matrix X;          // X represents the input to any model
+	matrix y;			// Y represents the output of any model
+	
+}data;
+
+// METHODS USEFUL
+data random_batch(data d, int n);
+list *get_lines(char *filename);
+data load_classification_data(char *images, char *label_file, int bias);
+char *fgetl(FILE *fp);
+void free_data(data d);
