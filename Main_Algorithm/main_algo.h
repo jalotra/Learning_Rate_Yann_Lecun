3ab6110195ab4517279773d36574e6a7b27c2a4f
Shivam <jalotrashivam9@gmail.com>
2019-11-09 18:32:48 +0530

    Changed calculate_delta_norm function signature

--- Main_Algorithm/main_algo.h
+++ Main_Algorithm/main_algo.h
@@ -10,7 +10,7 @@ double calculate_norm(matrix);
 matrix create_psi(int, int, double);
 matrix normalise_psi(matrix);
 matrix running_average(matrix, matrix, matrix);
-int calculate_delta_norm(double , matrix, matrix, matrix );
+int calculate_delta_norm(double ,matrix);
 
 // NOW FOR calculate_gradients.c
 void calculate_gradient1(model, matrix, data);
