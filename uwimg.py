e63b791b083635cf16a3853f0302cc9db26e9f95
Shivam <jalotrashivam9@gmail.com>
2019-11-09 18:34:35 +0530

    Added python wrapper funtion to the lib_shared_onj

--- uwimg.py
+++ uwimg.py
@@ -3,23 +3,78 @@ import math
 import random
 import os
 
-test_photos_directory = "test_photos/"
 
 def c_array(ctype, values):
     arr = (ctype*len(values))()
     arr[:] = values
     return arr
 
-class IMAGE(Structure):
-    _fields_ = [("w", c_int),
-                ("h", c_int),
-                ("c", c_int),
-                ("data", POINTER(c_float))]
+# class IMAGE(Structure):
+#     _fields_ = [("w", c_int),
+#                 ("h", c_int),
+#                 ("c", c_int),
+#                 ("data", POINTER(c_float))]
 
 
 #lib = CDLL("/home/pjreddie/documents/455/libuwimg.so", RTLD_GLOBAL)
 lib = CDLL(os.path.join(os.getcwd(), "libuwimg.so"), RTLD_GLOBAL) 
 
+
+
+# LETS DEFINE ALL THE LIBRARY FUNCTIONS THAT THE YANN LECUN PAPER WILL USE 
+class IMAGE(Structure):
+    _fields_ = [("w", c_int),
+                ("h", c_int),
+                ("c", c_int),
+                ("data", POINTER(c_float))]
+    def __add__(self, other):
+        return add_image(self, other)
+    def __sub__(self, other):
+        return sub_image(self, other)
+
+class POINT(Structure):
+    _fields_ = [("x", c_float),
+                ("y", c_float)]
+
+class DESCRIPTOR(Structure):
+    _fields_ = [("p", POINT),
+                ("n", c_int),
+                ("data", POINTER(c_float))]
+
+class MATRIX(Structure):
+    _fields_ = [("rows", c_int),
+                ("cols", c_int),
+                ("data", POINTER(POINTER(c_double))),
+                ("shallow", c_int)]
+
+class DATA(Structure):
+    _fields_ = [("X", MATRIX),
+                ("y", MATRIX)]
+
+(RELU, LRELU, SOFTMAX) = range(3    )
+class LAYER(Structure):
+    _fields_ = [("in", MATRIX),
+                ("dw", MATRIX),
+                ("w", MATRIX),
+                ("v", MATRIX),
+                ("out", MATRIX),
+                ("G1", MATRIX),
+                ("G2", MATRIX),
+                ("activation", c_int)]
+
+class MODEL(Structure):
+    _fields_ = [("layers", POINTER(LAYER)),
+                ("n", c_int)]
+
+
+
+def make_model(layers):
+    m = MODEL()
+    m.n = len(layers)
+    m.layers = (LAYER*m.n) (*layers)
+    return m
+
+#######################################################################
 make_image = lib.make_image
 make_image.argtypes = [c_int, c_int, c_int]
 make_image.restype = IMAGE
@@ -40,6 +95,7 @@ save_image_lib.argtypes = [IMAGE, c_char_p]
 def save_image(im, f):
     return save_image_lib(im, f.encode('ascii'))
 
+
 '''
 Using METHODS DEFINED IN THE process_image.c '''
 get_pixel = lib.get_pixel
@@ -53,17 +109,56 @@ set_pixel.argtypes = [IMAGE, c_int, c_int, c_int, c_float]
 copy_image = lib.copy_image
 copy_image.argtypes = [IMAGE]
 copy_image.restype = IMAGE
+# ###########################################################################
 
-# CReating a test_photos named folder
-def create_test_photos_directory():    
-    try:
-        os.mkdir(path = os.path.join(os.getcwd(), test_photos_directory), mode = 777 )
-    except FileExistsError:
-        pass 
 
+
+#####################################################################################
+# NUERAL NETWORK STUFF
+forward_model = lib.forward_model
+forward_model.argtypes = [MODEL, MATRIX]
+forward_model.restype = MATRIX
+
+load_classification_data = lib.load_classification_data
+load_classification_data.argtypes = [c_char_p, c_char_p, c_int]
+load_classification_data.restype = DATA
+
+make_layer = lib.make_layer
+make_layer.argtypes = [c_int, c_int, c_int]
+make_layer.restype = LAYER
+
+# For the claculation of gradients
+calculate_G1 = lib.calculate_gradient1
+calculate_G1.argtypes = [MODEL, MATRIX, DATA]
+calculate_G1.restype = None
+
+calculate_G2 = lib.calculate_gradient2
+calculate_G2.argtypes = [MODEL, MATRIX, DATA, MATRIX]
+calculate_G2.restype = None
+
+# FInding the Runnig avaregae of Psi
+# matrix running_average(matrix, matrix, matrix);
+running_average = lib.running_average
+running_average.argtypes = [MATRIX, MATRIX, MATRIX]
+running_average.restype = MATRIX
+
+# For calculate the norm of psi AT each step
+# int calculate_delta_norm(double norm1, matrix psi)
+calculate_delta_norm = lib.calculate_delta_norm
+calculate_delta_norm.argtypes = [c_double, MATRIX]
+calculate_delta_norm.restype = c_double 
+
+# TO define PSI
+# matrix create_psi(int rows, int cols, double s)
+# {
+#     return random_matrix(rows, cols, s);
+# }
+create_psi = lib.create_psi
+create_psi.argtypes = [c_int, c_int, c_double]
+create_psi.restype = MATRIX
 
 if __name__ == "__main__":
-    create_test_photos_directory()
+    # create_test_photos_directory()
     # Testing pre-build methods
     im = load_image("data/dog.jpg")
     save_image(im, test_photos_directory + "hey")
